b9976fb14042bae1497d0b4d83a21abe36268d78
XoLeRyTeR <122359148+XoLeRyTeR@users.noreply.github.com>
2024-10-25 18:12:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from models.train_module import TrainModule
-from utils import initialize_logging, load_config
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+sidebar=st.sidebar.selectbox('страницы',('lupa','zalupa'))
 
-config = load_config(config_path="./config/config.yaml")
-initialize_logging(config_path="./config/logging_config.yaml", debug=False)
-trainer = TrainModule(config)
-trainer.pipeline()
-trainer.test()
+if sidebar=='lupa':
+     st.write('Hello, *World!* :sunglasses:')
+     st.header('st.button')
+     but = st.button('ид')
+     hut = st.button('и')
+     if but:
+          sidebar='zalupa'
+          st.write('Why hello there')
+     elif hut:
+          st.write('Goodbye')
+else:
+     columns = st.columns(2)
+     with columns[0]:
+          st.metric('Сколько камней в кг', 72, 3)
+     with columns[1]:
+          st.metric('Сколько воды в л', 424, -6)
+     df = pd.DataFrame(sns.load_dataset('iris'))
+     st.write(df)
+     '''
+     fig, ax = plt.subplots()
+     sns.scatterplot(x='sepal_length', y='sepal_width', data=data, ax=ax)
+     st.write("График разброса:")
+     st.pyplot(fig)'''
+
